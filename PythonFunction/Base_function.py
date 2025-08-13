fix lift amplitude computation + add ankle height stat

PythonFunction/Base_function.py
@@ -152,7 +152,7 @@ def analyze_intervals_duration(
         # amplitude if requested
         if use_distance:
             seg = distance[start : end + 1]  # end inclusive
-            amp = float(np.ptp(seg))  # max - min
+            amp = float(np.max(seg) - 100)  # max - threshold
             amplitude_per_event.append(amp)
 
     if not durations_per_event:  # aucune fenêtre valide

PythonFunction/Leg_lift_adduct.py
@@ -257,7 +257,7 @@ def ankle_high(
         common_intervals, time_vector, ankle_high_from_ground
     )
 
-    return lift_with_leg_extend, distance_pelv_ank
+    return lift_with_leg_extend, distance_pelv_ank, ankle_high_from_ground
 
 
 def compute_distance(coords1: np.ndarray, coords2: np.ndarray) -> np.ndarray: